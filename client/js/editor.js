8e3b6a5135d42b04ec4b2f962383441f4ddfc171
David Peterman <peterman.dd@gmail.com>
2023-03-17 13:26:36 -0400

    💄 Finished basic UI for editor page.

--- client/js/editor.js
+++ client/js/editor.js
@@ -1,17 +1,41 @@
+
+
+
+
 window.addEventListener('load', function() {
-    
-    const contentEditable = document.querySelector('#contenteditable');
-
-    contentEditable.addEventListener('keyup', function(e) {
-        console.log('keyuped');
-    })
-
-    contentEditable.addEventListener('paste', function(e) {
-        console.log('pasted');
-    })
-
-    contentEditable.addEventListener('input', function(e) {
-        console.log('inputed');
-    })
-    //contentEditable.click();
+    setEventListeners();
+    getLocalStorage();
 })
+
+
+function setEventListeners() {
+    const contentEditable = document.querySelector('#contenteditable');
+    contentEditable.addEventListener('keyup', function(e) {
+        setLocalStorage();
+    });
+    contentEditable.addEventListener('paste', function(e) {
+        setLocalStorage();
+    });
+    contentEditable.addEventListener('input', function(e) {
+        setLocalStorage();
+    });
+}
+
+function getLocalStorage() {
+    const contentEditable = document.querySelector('#contenteditable');
+    let existingData = JSON.parse(window.localStorage.getItem('anonymous-docs-text-data'));
+    
+    if (!existingData) {
+        existingData = "";
+        window.localStorage.setItem('anonymous-docs-text-data', JSON.stringify(existingData));
+    }
+    else {
+        contentEditable.innerHTML = existingData;
+    } 
+}
+
+
+function setLocalStorage() {
+    const contentEditable = document.querySelector('#contenteditable');
+    window.localStorage.setItem('anonymous-docs-text-data', JSON.stringify(contentEditable.innerHTML));
+}
