d863b4726ed4fda26b943f16f31134670d66a242
David Peterman <peterman.dd@gmail.com>
2023-03-18 12:52:34 -0400

    ✨💄 Add 'marked' library for live text -> md conversion. UI improvements.

--- client/js/editor.js
+++ client/js/editor.js
@@ -9,20 +9,30 @@ window.addEventListener('load', function() {
 
 
 function setEventListeners() {
-    const contentEditable = document.querySelector('#contenteditable');
+    const contentEditable = document.querySelector('#md-textarea');
     contentEditable.addEventListener('keyup', function(e) {
         setLocalStorage();
+        updatePreview();
     });
     contentEditable.addEventListener('paste', function(e) {
         setLocalStorage();
+        updatePreview();
     });
     contentEditable.addEventListener('input', function(e) {
         setLocalStorage();
+        updatePreview();
     });
 }
 
+
+function updatePreview() {
+    const text = document.getElementById('md-textarea').value;
+    document.getElementById('md-preview').innerHTML = marked.parse(text);
+}
+
+
 function getLocalStorage() {
-    const contentEditable = document.querySelector('#contenteditable');
+    const contentEditable = document.querySelector('#md-textarea');
     let existingData = JSON.parse(window.localStorage.getItem('anonymous-docs-text-data'));
     
     if (!existingData) {
@@ -31,11 +41,12 @@ function getLocalStorage() {
     }
     else {
         contentEditable.innerHTML = existingData;
+        updatePreview();
     } 
 }
 
 
 function setLocalStorage() {
-    const contentEditable = document.querySelector('#contenteditable');
+    const contentEditable = document.querySelector('#md-textarea');
     window.localStorage.setItem('anonymous-docs-text-data', JSON.stringify(contentEditable.innerHTML));
 }
