9afdab3cf94f71fa1a57ba27474e500dadbe4e68
lawrenceberardelli <lawrence.berardelli@gmail.com>
2023-03-18 20:19:03 -0400

    adding model and control

--- src/main/java/ca/mcgill/documentserver/model/User.java
+++ src/main/java/ca/mcgill/documentserver/model/User.java
@@ -12,5 +12,15 @@ public class User {
   //TODO: Idea for document editor: split the document into sections and allow one user at a time to claim a section.
   //TODO: In combination with a chat app which can reference other sections, comments can be enabled. 
   //TODO: It's actually a private message board... kinda stole the idea from a group already. 
+  
+  public User() {}
+  
+  public User(String username) {
+    this.username = username;
+  }
+  
+  public String getUserName() {
+    return username;
+  }
 
 }
