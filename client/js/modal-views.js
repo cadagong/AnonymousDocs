0035f200dec150fc10507e1513413cadac26b5c3
David Peterman <peterman.dd@gmail.com>
2023-03-21 19:48:58 -0400

    Add dashboard.html

---
+++ client/js/modal-views.js
@@ -0,0 +1,12 @@
+const COLLAB_SETTINGS = 
+    `<div id="modal-content-add-collab" class="modal-content-subsection">
+        <div id="modal-content-subtitle-add" class="modal-content-subtitle">Add Collaborators</div>
+        <div id="modal-content-subsection-left"></div>
+    </div>
+    <div id="modal-content-existing-collabs" class="modal-content-subsection">
+        <div id="modal-content-subtitle-existing" class="modal-content-subtitle">Update Existing Collaborators</div>
+        <div id="modal-content-subsection-right"></div>
+    </div>`;
+
+const EDIT_DOCUMENT_SECTION = 
+    ``;
